efb7fad8235ddfb4047ca81a2dc3db4d78e49836
crosssh <crosssh@oregonstate.edu>
2017-01-24 11:19:58 -0800

    updating bucket key

--- html/js/vr-party-presenter.js
+++ html/js/vr-party-presenter.js
@@ -2,7 +2,7 @@ var _socket = io();
 var _sessionId;
 var _viewer;
 var _last_distance_to_target;
-var _view_data_bucket = 'Forge VR';
+var _view_data_bucket = 'forgevr';
 var _default_models = {
     'pumpkin'       : 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dnJwYXJ0eS9wdW1rcGluX3YxLjcyODk1MjAzLTIzZTQtNGRkZi05MThlLWZhNjliOTlhYWJiOC5mM2Q',
     'robot arm'     : 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6c3RlYW1idWNrL1JvYm90QXJtLmR3Zng=',
@@ -21,6 +21,7 @@ var _default_models = {
     //'trophy'        : 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dnJwYXJ0eS9Ucm9waHlfQW5nZWxIYWNrLmYzZA==',
     //'cake'          : 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dnJwYXJ0eS9IQkM0LmR3Zng='
     'movement'      : 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dnJwYXJ0eS9FVEFfNjQ5Ny0xX01vdmVtZW50X0NvcnJlY3RlZF8zLmR3Zg'
+	'sports car'	: 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Zm9yZ2V2ci9TcG9ydHMlMjBDYXIuZHdmeA'
 };
 var _hosts = [ 'murmuring-cove-16220.herokuapp.com'];
 
