839c56225f40c83d0a1cd07efd0590282e986f96
Paul Kwak <kwakp@oregonstate.edu>
2017-01-30 18:21:44 -0800

    Conflict with different URL and tweaks

--- html/js/auth_test.js
+++ html/js/auth_test.js
@@ -1,26 +1,26 @@
-var URL = window.location.toString();
+var Auth_URL = window.location.toString();
 
 // Splits this https://murmuring-cove-16220.herokuapp.com/?code=QF2BHBpC6ZMh2PszqRpvKoMIdnSjzPbvu1r-uQId
 // ------------------------------------------------------
 // into https://murmuring-cove-16220.herokuapp.com/
 // and code=QF2BHBpC6ZMh2PszqRpvKoMIdnSjzPbvu1r-uQId
 
-var checker = URL.split( '?' );
+var checker = Auth_URL.split( '?' );
 
 // Splits this code=QF2BHBpC6ZMh2PszqRpvKoMIdnSjzPbvu1r-uQId
 // ------------------------------------------------------
 // into code
 // and QF2BHBpC6ZMh2PszqRpvKoMIdnSjzPbvu1r-uQId
 
-var Auth_Code = checker[1].split( '=' );
+var Auth_Code_Parts = checker[1].split( '=' );
 
 // Should return "code"
-console.log(Auth_Code[0]);
+console.log(Auth_Code_Parts[0]);
 
-if (Auth_Code[0] == "code") {
+if (Auth_Code_Parts[0] == "code") {
 	
 	//The actual code we can now use.
-	Auth_code = Auth_Code[1]
+	var Auth_code = Auth_Code_Parts[1]
 	console.log(Auth_Code);
 	
 }
