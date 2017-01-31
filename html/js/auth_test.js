4cc7332e3e2814c4397d75382ed7d51ee17be2f4
Paul Kwak <kwakp@oregonstate.edu>
2017-01-30 18:14:09 -0800

    maybe last?

--- html/js/auth_test.js
+++ html/js/auth_test.js
@@ -1,14 +1,9 @@
-var URL = window.location;
-
-//should print full url 
-console.log(URL);
-
 // Splits this https://murmuring-cove-16220.herokuapp.com/?code=QF2BHBpC6ZMh2PszqRpvKoMIdnSjzPbvu1r-uQId
 // ------------------------------------------------------
 // into https://murmuring-cove-16220.herokuapp.com/
 // and code=QF2BHBpC6ZMh2PszqRpvKoMIdnSjzPbvu1r-uQId
 
-var checker = URL.split( '?' );
+var checker = window.location.split( '?' );
 
 // Splits this code=QF2BHBpC6ZMh2PszqRpvKoMIdnSjzPbvu1r-uQId
 // ------------------------------------------------------
