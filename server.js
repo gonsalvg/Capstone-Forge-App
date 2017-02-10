babc4f62d0e6aeab601e6dabfbf9cb7e13510c2c
Paul Kwak <kwakp@oregonstate.edu>
2017-02-09 17:01:14 -0800

    test again

--- server.js
+++ server.js
@@ -1,10 +1,72 @@
+/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+'use strict';
+
 var express = require('express');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var app = express();
+
+// this session will be used to save the oAuth token
+app.use(cookieParser());
+app.set('trust proxy', 1) // trust first proxy - HTTPS on Heroku 
+app.use(session({
+    secret: 'autodeskforge',
+    cookie: {
+        httpOnly: true,
+        secure: (process.env.NODE_ENV === 'production'),
+        maxAge: 1000 * 60 * 60 // 1 hours to expire the session and avoid memory leak
+    },
+    resave: false,
+    saveUninitialized: true
+}));
+
+//var express = require('express');
 var api = require('./api');
 var http = require('http');
 var crypto = require('crypto');
 
+//#################Adams Code#############################
+//###############################################################
+// prepare server routing
+app.use('/', express.static(__dirname + '/../www')); // redirect static calls
+app.use('/js', express.static(__dirname + '/../node_modules/bootstrap/dist/js')); // redirect static calls
+app.use('/js', express.static(__dirname + '/../node_modules/jquery/dist')); // redirect static calls
+app.use('/css', express.static(__dirname + '/../node_modules/bootstrap/dist/css')); // redirect static calls
+app.use('/fonts', express.static(__dirname + '/../node_modules/bootstrap/dist/fonts')); // redirect static calls
+//app.set('port', process.env.PORT || 3000); // main port// Already using port 5000
 // CONFIG
+
+////////////////////////////////
 var port = process.env.PORT || 5000;
+////////////////////////////////
+
+// prepare our API endpoint routing
+var oauth = require('./oauth');
+var dm = require('./data.management');
+var md = require('./model.derivative');
+app.use('/', oauth); // redirect oauth API calls
+app.use('/dm', dm); // redirect our Data Management API calls
+app.use('/md', md); // redirect our Data Management API calls
+
+module.exports = app;
+//###############################################################
+//###############################################################
 
 // WEB SERVER
 var app = express();
@@ -20,9 +82,7 @@ app.use('/', express.static(__dirname + '/html'));
 app.use('/v2', express.static(__dirname + '/html/index2.html'));
 
 app.get('/api/token', api.getToken);
-
 app.get('/api/uploadtoken', api.getUploadToken);
-
 app.get('/api/sessionId', function(req, res) {  
     var sessionId;  
     do {
